b068a3237d5aa5a478d47da8cec32b3f60ea4763
constance scott <constancedscott05@yahoo.com>
2023-05-27 22:46:12 -0500

    add new files

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let username = prompt ('What is your name')
+console.log('Hi there ' + username)
+
+let time = prompt('What hour is it? (0-23)');
+
+while(userResponse !== 'boxer'){
+    prompt('What is your favorite dog?')
+}
